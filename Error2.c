62e06d085dce0788f96d6b07939cec071e576e3c
The-Dotcom-code <142333668+The-Dotcom-code@users.noreply.github.com>
2023-10-17 10:05:08 -0600

    Add files via upload

---
+++ Error2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float perimetroFigura(float numero){
+  float perimetro;
+  perimetro= 4*numero;
+  return (perimetro);
+
+}
+
+float areafigura(float numero){
+  float area;
+  area=numero*numero;
+  return(area);
+}
+
+int main(){
+  float num;
+  printf("Lado de un cuadrado: ");
+  scanf(" %f", &num);
+  printf("Perimetro: %f\n", perimetroFigura(num));
+  printf("Area: %f\n", areafigura(num));
+  return 0;
+}
